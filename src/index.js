64f61f90e5fe7c63c185ea79efb092828f723355
Egor-Leshkevich <dellg3172018@gmail.com>
2021-03-15 16:12:23 +0300

    feat: implement task function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  var arr = [];
+  var string;
+  var number;
+  arr= Array.from(String(n), Number);
+  arr.reverse();
+  string=arr.join('');
+  number=parseInt(string,10);
+  return number;
 }
